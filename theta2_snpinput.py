db374413985ffe30465f2e5f49ee9598266bc84f
patterja <patterja@ohsu.edu>
2015-12-30 14:56:38 -0800

    Theta2 for tumor purity needs tumor SNP file and normal SNP file. Format 1: A tab delimited file with columns     (1) chromosome     (2) position     (3) ref_allele - number of reads with the reference allele.     (4) mut_allele - number of reads with the mutant allele.

---
+++ theta2_snpinput.py
@@ -0,0 +1,20 @@
+__author__ = 'Janice'
+
+import os
+import re
+import pandas as pd
+
+newdir = "C:\Users\Owner\Box Sync\theta2\\"
+snpdir = "C:\Users\Owner\Box Sync\mutect_output\mutect_output_raw\\"
+
+
+for file in os.listdir(snpdir):
+    if file.endswith("callstats.txt"):
+        snpmutect=pd.read_table(snpdir + file, header=1, sep="\t")
+        snpfilter=snpmutect[(snpmutect.judgement=="KEEP")]
+        tumor=snpfilter[['contig','position','t_ref_count','t_alt_count']]
+        normal=snpfilter[['contig','position','n_ref_count','n_alt_count']]
+for file in os.listdir(snpdir):
+    if file.endswith("callstats.txt"):
+        tumor.to_csv(newdir +  re.sub(r"dedup_realigned_recalib_callstats.txt$|merged_sort_callstats.txt$", 'tumor_SNP.txt', file))
+        normal.to_csv(newdir + re.sub(r"dedup_realigned_recalib_callstats.txt$|merged_sort_callstats.txt$", 'norm_SNP.txt', file))
