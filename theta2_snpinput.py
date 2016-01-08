30900962e3de2aa7b4a05e5888b447a596a59598
patterja <patterja@ohsu.edu>
2016-01-07 18:06:58 -0800

    Adjusted to add hashtag at first line, first row. Chromosome name changed from "chr1" to "1", remove "chr"

--- theta2_snpinput.py
+++ theta2_snpinput.py
@@ -12,8 +12,10 @@ for file in os.listdir(snpdir):
     if file.endswith("callstats.txt"):
         snpmutect=pd.read_table(snpdir + file, header=1, sep="\t")
         snpfilter=snpmutect[(snpmutect.judgement=="KEEP")]
-        tumor=snpfilter[['contig','position','t_ref_count','t_alt_count']]
-        normal=snpfilter[['contig','position','n_ref_count','n_alt_count']]
+        snpfilter['contig']=snpfilter['contig'].str[3:]
+        snpfilter=snpfilter.rename(columns = {'contig':'#contig'})
+        tumor=snpfilter[['#contig','position','t_ref_count','t_alt_count']]
+        normal=snpfilter[['#contig','position','n_ref_count','n_alt_count']]
 
         tumor.to_csv(newdir +  re.sub(r"dedup_realigned_recalib_callstats.txt$|merged_sort_callstats.txt$", 'tumor_SNP.txt', file), sep="\t", index=False)
         normal.to_csv(newdir + re.sub(r"dedup_realigned_recalib_callstats.txt$|merged_sort_callstats.txt$", 'norm_SNP.txt', file), sep="\t", index=False)
