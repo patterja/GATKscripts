0baf0b603518fca829e3bb25e641f3e9709ed34f
patterja <patterja@ohsu.edu>
2016-06-16 17:03:41 -0700

    data frames now document allelic fraction and depth

--- clean_MAF.py
+++ clean_MAF.py
@@ -1,30 +1,33 @@
 #!/usr/bin/env python
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 Output MAF file from Oncotator output.
-Usage:
-python clean_MAF.py
+Usage: python clean_MAF.py
+#
+#Modules can be used, not used because Indel and SNP mutect output are completely different
+#because I'm cursed. Something fixable, when I'm smarter.
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 import os
 import re
 import pandas as pd
 
-newdir = "C:\Users\Owner\Box Sync\mutsig\\"
+newdir = "C:\Users\Owner\Box Sync\mutsig\inputs\\"
 snpdir = "C:\Users\Owner\Box Sync\oncotator\oncotator_snps\\"
 indeldir = "C:\Users\Owner\Box Sync\oncotator\oncotator_indel\\"
 
 for subdir, dirs, files in os.walk(snpdir):
-    #input the directory you want to walk through full of the oncotate files
-    fw = open(newdir + "gistsnp_somatic_oncotate_unlabel.txt", 'w')
-    #fw = open(newdir + file.replace("_merged_sort_mutect_filt_annotated", "_somatic_oncotate"), 'w')
+    #input the directory you want to walk through full of the oncotate files this is the raw everything file
     fh = open(snpdir + files[1], 'r')
     lines = fh.readlines()
-    fw.write(str(lines[100]))#write column names/header into the master file
+    # write column names/header into the master file
+    fw = open(newdir + "gistsnp_somatic_oncotate_unlabel.txt", 'w')
+    fw.write(str(lines[100]))
     fh.close()
     norm_match=[]
     tumor_samp=[]
     for file in files:
         fh = open(snpdir + file, 'r')  # use the absolute URL of the file
         lines = fh.readlines()
+        #skip the vcf header that oncotator wrote out into the MAF file
         for line in lines[101:]:
             field = re.split(r'\t', line)
             if int(field[302]) == 2:
@@ -42,13 +45,15 @@ for subdir, dirs, files in os.walk(snpdir):
 snpdat=  pd.read_csv(newdir + "gistsnp_somatic_oncotate_unlabel.txt", header=0, sep="\t", dtype=object)
 snpdat.Tumor_Sample_Barcode=tumor_samp
 snpdat.Matched_Norm_Sample_Barcode=norm_match
-#cols = snpdat.columns.tolist()
 snpdat.to_csv(newdir + "gistsnp_somatic_oncotate.txt", sep="\t", dtype=object, index=False)
 #only use snps where read depth was equal or above 20
-#snpdat=snpdat[snpdat['read_depth'] > 19]
-#snpdat.to_csv(newdir + "gistsnp_somatic_oncotate_q20.maf.txt", sep="\t")
+#   snpdat=snpdat[snpdat['read_depth'] > 19]
+#   snpdat.to_csv(newdir + "gistsnp_somatic_oncotate_q20.maf.txt", sep="\t")
 
-snpdat_trunc = snpdat.ix[:,'Hugo_Symbol':'Protein_Change']
+#column 80 is t_ref_count and column 199=allelic depth
+snpdat_trunc = snpdat[snpdat.columns[range(42)+[79,199]]]
+snpdat_trunc.columns.values[[42,43]]=['T_AC','T_DP']
+#snpdat_trunc = snpdat.ix[:,'Hugo_Symbol':'Protein_Change']
 cond = snpdat_trunc['Tumor_Sample_Barcode'].str.contains('^RK|^SUR')
 
 snpdatkit = snpdat_trunc[~cond]
@@ -94,9 +99,9 @@ indeldat.to_csv(newdir + "gistsindel_somatic_oncotate.txt", sep="\t", dtype=obje
 
 #only use snps where read depth was equal or above 20
 #indeldat=indeldat[indeldat['T_DP'] > 19]
-#indeldat.to_csv(newdir + "gistsindel_somatic_oncotate_q20.maf.txt", sep="\t")
-
-indel_trunc = indeldat.ix[:,'Hugo_Symbol':'Protein_Change']
+#columns 198, 200 have AC (allelic count) and Depth
+indel_trunc=indeldat[indeldat.columns[range(42)+[198,199]]]
+#indel_trunc = indeldat.ix[:,'Hugo_Symbol':'Protein_Change']
 cond = indel_trunc['Tumor_Sample_Barcode'].str.contains('^RK|^SUR')
 
 indelkit = indel_trunc[~cond]
@@ -105,6 +110,7 @@ indelrk = indel_trunc[cond]
 
 
 #Combining the snps and indels into a single mafs
+#Change snpdatkit alt count and read depth to match indels
 
 mutdatkit = pd.concat([snpdatkit, indelkit])
 mutdatkit.to_csv(newdir + "combo_muts_KIT.maf.txt", sep="\t", dtype=object, index=False)
